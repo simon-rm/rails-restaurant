89aea389ddc2485f9e388158a9819345076218c6
Simon R. Murcia <srisnikmurcia@itba.edu.ar>
2024-03-05 09:28:05 -0300

    added address column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_05_121538) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_05_122535) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
 end
